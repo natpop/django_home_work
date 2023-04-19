8dc51cb441444751d7df990263587c637da682d4
Nikita <popov.nikita2807@gmail.com>
2023-04-19 14:39:14 +0300

    Выполнены задания блока 3 практика ORMч2

--- MainApp/models.py
+++ MainApp/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Item(models.Model):
     name  = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
-    count = models.PositiveIntegerField() 
+    count = models.PositiveIntegerField()
+    desc = models.CharField(max_length=1500, default='')
     #def __str__(self) -> str:
      # return self.name, self.brand, self.count
