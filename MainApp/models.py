499fefb55b960a332f3704804158bc5508364345
Nikita <popov.nikita2807@gmail.com>
2023-04-20 09:03:07 +0300

    Добавление связанной таблиц

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 
+class Color(models.Model):
+   name  = models.CharField(max_length=20)
+   def __str__(self) -> str:
+      return f"{self.name}"
+
 class Item(models.Model):
     name  = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
     desc = models.CharField(max_length=1500, default='')
-    #def __str__(self) -> str:
-     # return self.name, self.brand, self.count
+    colors = models.ManyToManyField(to=Color)
+    def __str__(self) -> str:
+      return f"{self.name, self.brand, self.count}"
+
+
+
